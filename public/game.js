c93694fd42309bb02ed2a29661a9ba949b475cec
puneet <puneet.prajapati@contriverz.com>
2021-05-26 18:25:00 +0530

    ffg

---
+++ public/game.js
@@ -0,0 +1,25 @@
+//import {Phaser} from 'phaser';
+const config = {
+  width: 800,
+  height: 600,
+  type: Phaser.AUTO,
+  audio: {
+    disableWebAudio: true
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      fps: 60,
+      gravity: {y : 0},
+    }
+  },
+};
+
+const game = new Phaser.Game(config);
+game.preload = function(){
+  this.load.image('background',download.jpeg);
+}
+
+game.create = function(){
+  this.add.sprite(0,0,'background');
+}
